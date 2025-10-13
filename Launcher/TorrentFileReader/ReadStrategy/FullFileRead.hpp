6d547cf97c486b358ab0fb6ef018fa3f2c8a7ba8
brano-san <branoverv10@bk.ru>
2025-10-13 22:41:02 +0300

    Add torrent file reader files

---
+++ Launcher/TorrentFileReader/ReadStrategy/FullFileRead.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "IReadStrategy.hpp"
+
+class TorrentFileReaderStrategy : IReadStrategy
+{
+public:
+    TorrentFileReaderStrategy() = default;
+    ~TorrentFileReaderStrategy() = default;
+
+    std::vector<uint8_t> read() override;
+};
